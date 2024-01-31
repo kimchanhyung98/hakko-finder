5f7a5d70ce2232b7b5ac2ab54cb411bbc8356501
kimchanhyung98 <kimchanhyung98@gmail.com>
2024-01-31 15:30:56 +0900

    feature: random streaming

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,51 +1,23 @@
 <script lang="ts">
-    import { onMount } from 'svelte';
-    import { fetchChzzk } from '../utils/stream';
-    import { checkNeedUpdate } from '../utils/storage';
-    import { fetchChzzkData } from '../utils/chzzkService';
-    import type { MinorStream, NextPage } from '../types/chzzk';
-
-    const fetchData = async (nextPage?: NextPage) => {
-        try {
-            return await fetchChzzk(nextPage);
-        } catch (error) {
-            console.error('Error fetching data:', error);
-        }
-    };
+    import {onMount} from 'svelte';
+    import {checkNeedUpdate, updateStreamData} from '../utils/storage';
 
     onMount(() => {
-        if (checkNeedUpdate()) {
-            let allStream: MinorStream[] = [];
-            let nextPage: NextPage | undefined;
+        if (checkNeedUpdate()) updateStreamData();
+        const streamData = JSON.parse(localStorage.stream_data);
 
-            for (let i = 0; i < 5; i++) {
-                /*
-                const data: { stream: MinorStream[]; nextPage: NextPage } | null = await fetchData(nextPage);
-                allStream.push(...data.stream);
-                nextPage = data.nextPage;
-                */
-            }
-
-            localStorage.stream_data = JSON.stringify(allStream);
-            localStorage.hakko_updated_at = Date.now();
-        }
+        const randomStream = streamData[Math.floor(Math.random() * streamData.length)];
+        const iframe = document.querySelector('iframe') as HTMLIFrameElement;
+        iframe.src = `https://chzzk.naver.com/live/${randomStream.id}`;
     });
 </script>
-
-<h1>Welcome to SvelteKit</h1>
-
-<div class="iframe-wrapper">
-    <iframe src="https://chzzk.naver.com/live/6e06f5e1907f17eff543abd06cb62891" title="chzzk"
-            width="100%" height="1000px" allow="autoplay" allowfullscreen>
-    </iframe>
-</div>
 
 <style>
     .iframe-wrapper {
         overflow: hidden;
         position: relative;
         width: 100%;
-        max-width: 1050px;
+        max-width: 770px;
         min-width: 580px;
         aspect-ratio: 16 / 9.005;
         margin: 0 auto;
@@ -61,3 +33,7 @@
         width: calc(104% + 397px);
     }
 </style>
+
+<div class="iframe-wrapper">
+    <iframe src="" title="chzzk" width="100%" height="1000px" allow="autoplay" allowfullscreen></iframe>
+</div>
--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,3 +1,6 @@
+import type {MinorStream, NextPage} from "../types/chzzk";
+import {fetchChzzk} from "./stream";
+
 export function checkNeedUpdate() {
     if (!localStorage.hakko_updated_at) {
         return true;
@@ -7,4 +10,25 @@ export function checkNeedUpdate() {
 
         return Date.now() - updatedAt > interval;
     }
+}
+
+export async function updateStreamData() {
+    const allStream: MinorStream[] = [];
+    try {
+        let nextPage: NextPage | undefined;
+        for (let i = 0; i < 10; i++) {
+            const data: { stream: MinorStream[]; nextPage: NextPage } | null = await fetchChzzk(nextPage);
+            if (data === null) continue;
+
+            allStream.push(...data.stream);
+            if (allStream.length > 50) break;
+
+            nextPage = data.nextPage;
+        }
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+
+    localStorage.stream_data = JSON.stringify(allStream);
+    localStorage.hakko_updated_at = Date.now();
 }
