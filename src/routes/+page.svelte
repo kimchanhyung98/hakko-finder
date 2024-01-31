1e522f3266efe5c3e6aca7c766f6753178eecef2
LeeByeongMuk <mansogod@gmail.com>
2024-01-31 14:51:22 +0900

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
     import { onMount } from 'svelte';
     import { fetchChzzk } from '../utils/stream';
     import { checkNeedUpdate } from '../utils/storage';
+    import { fetchChzzkData } from '../utils/chzzkService';
     import type { MinorStream, NextPage } from '../types/chzzk';
 
     const fetchData = async (nextPage?: NextPage) => {
@@ -33,6 +34,30 @@
 
 <h1>Welcome to SvelteKit</h1>
 
-<!--<iframe src="https://chzzk.naver.com/live/4d0b7d3f825ea982b95f0a5c2b4782d3" title="chzzk"-->
-<!--        width="100%" height="1000px" allow="autoplay" allowfullscreen>-->
-<!--</iframe>-->
+<div class="iframe-wrapper">
+    <iframe src="https://chzzk.naver.com/live/6e06f5e1907f17eff543abd06cb62891" title="chzzk"
+            width="100%" height="1000px" allow="autoplay" allowfullscreen>
+    </iframe>
+</div>
+
+<style>
+    .iframe-wrapper {
+        overflow: hidden;
+        position: relative;
+        width: 100%;
+        max-width: 1050px;
+        min-width: 580px;
+        aspect-ratio: 16 / 9.005;
+        margin: 0 auto;
+        border: 1px solid #e6e6e6;
+    }
+
+    .iframe-wrapper iframe {
+        overflow: hidden;
+        position: absolute;
+        top: -62px;
+        left: -70px;
+        z-index: 100;
+        width: calc(104% + 397px);
+    }
+</style>
