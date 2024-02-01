6e45b670b94f51383813837ef49f9c0c36ea32c6
LeeByeongMuk <mansogod@gmail.com>
2024-02-01 18:25:39 +0900

    style: initial layout style

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,11 +17,10 @@
         overflow: hidden;
         position: relative;
         width: 100%;
-        max-width: 770px;
         min-width: 580px;
         aspect-ratio: 16 / 9.005;
         margin: 0 auto;
-        border: 1px solid #e6e6e6;
+        border: 1px solid #00ffa3e6;
     }
 
     .iframe-wrapper iframe {
@@ -32,11 +31,44 @@
         z-index: 100;
         width: calc(104% + 397px);
     }
+
+    .stream-info {
+        margin-top: 20px;
+    }
+
+    .stream-info .stream-title {
+        overflow: hidden;
+        margin: 0;
+        font-size: 24px;
+        font-weight: bold;
+        line-height: normal;
+        color: #fff;
+        margin-bottom: 10px;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+    }
+
+    .stream-info .chanel-id {
+        font-size: 16px;
+        color: #fff;
+        text-decoration: none;
+    }
+
+    .stream-info .chanel-id:hover {
+        text-decoration: underline;
+    }
 </style>
 
 
 <div id="container">
     <div class="iframe-wrapper">
         <iframe src="" title="chzzk" width="100%" height="1000px" allow="autoplay" allowfullscreen></iframe>
+    </div>
+
+    <div class="stream-info">
+        <h4 class="stream-title">Title</h4>
+        <a class="chanel-id"
+           href="https://www.twitch.tv/" target="_blank"
+        >name</a>
     </div>
 </div>
